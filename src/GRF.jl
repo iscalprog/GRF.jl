b873c15c00f4ecb11a3cf80c806f291c37d166d9
ASaragga <47437960+ASaragga@users.noreply.github.com>
2021-03-14 06:03:51 +0000

    Sharing notebooks in an executable environment: Binder

--- src/GRF.jl
+++ src/GRF.jl
@@ -1,9 +1,10 @@
 module GRF
 
 # Partilha de notebooks
-# binder: https://mybinder.org
-# 
+# binder: https://mybinder.org 
+# https://mybinder.org/v2/gh/ASaragga/GRF.jl/HEAD
+#           
 # nbviewer: https://nbviewer.jupyter.org
-# 
+# https://github.com/ASaragga/GRF.jl/blob/master/src/Modulo1.1-1.3.ipynb
 
 end
