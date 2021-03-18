8e16748a12538c322f8701a7aa714fc09c126654
ASaragga <47437960+ASaragga@users.noreply.github.com>
2021-03-18 00:07:59 +0000

    update Modulo1.1-1.7.ipynb

--- src/GRF.jl
+++ src/GRF.jl
@@ -1,8 +1,16 @@
 module GRF
 
+using DelimitedFiles    # Leitura/escrita de ficheiros em formato csv (delimitados por ,) 
+using Statistics        # Estatística descritiva, inclui cálculo do desvio-padrão e quantis
+using Distributions     # Distribuições de probabilidade
+using HypothesisTests   # Testes de hipóteses
+using ARCHModels        # Modelos GARCH
+using StatsPlots        # Gráficos 
+
+
 # Partilha de notebooks
 # binder: https://mybinder.org 
-# https://mybinder.org/v2/gh/ASaragga/GRF.jl/HEAD
+# 
 #           
 # nbviewer: https://nbviewer.jupyter.org
 # https://github.com/ASaragga/GRF.jl/blob/master/src/Modulo1.1-1.7.ipynb
