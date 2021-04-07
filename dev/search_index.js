4aba792be02e4f4f44d7f9593c138d3e16cc6109
Documenter.jl <documenter@juliadocs.github.io>
2021-04-07 16:19:35 +0000

    build based on 068dd88

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GRF","category":"page"},{"location":"#GRF","page":"Home","title":"GRF","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GRF]","category":"page"}]
+}
